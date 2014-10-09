ec0f3c1274c7c0b1339dafddd1834740e3a2154c
ratiom <skippydoesntknow@gmail.com>
2014-10-08 20:32:52 -0400

    finished quiz1

--- module3/quiz1/quiz1_source.R
+++ module3/quiz1/quiz1_source.R
@@ -1,3 +1,6 @@
+setwd("C:/Users/Conor/git/dataSpecialization/")
+library(data.table)
+
 q1DF <- data.table(q1csv)
 #question1
 q1DF_val <- q1DF[q1DF$VAL=="24"]
@@ -12,4 +15,11 @@ library(XML)
 fileURL <- ("http://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Frestaurants.xml")
 doc <- xmlTreeParse(fileURL,useInternal=TRUE)
 rootNODE <- xmlRoot(doc)
-zips <- xpathSApply(rootNODE, "//li[@zipcode='21231']",xmlValue)
+zips <- xpathSApply(rootNODE, "//row[zipcode='21231']", xmlValue)
+
+#q5
+fileURL <- ("http://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Fss06pid.csv")
+DT <- fread(fileURL)
+race = 1000
+meas_tapply <- system.time(replicate(race,sapply(split(DT$pwgtp15,DT$SEX),mean)))
+# not sure, this didnt give the right answer for me, but didn't test all of the functions
